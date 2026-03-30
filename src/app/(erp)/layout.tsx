0cc27b69e95629be6982590d1a53712bd1269037
Integral Service <Contato@integralservice.net.br>
2026-03-30 13:35:44 -0300

    feat: AuthProvider and CarrinhoProvider

--- src/app/(erp)/layout.tsx
+++ src/app/(erp)/layout.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from "react";
 import Sidebar from "@/components/layout/Sidebar";
 import Topbar from "@/components/layout/Topbar";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function ERPLayout({ children }: { children: ReactNode }) {
   return (
@@ -12,7 +13,10 @@ export default function ERPLayout({ children }: { children: ReactNode }) {
       <div className="flex flex-1 flex-col">
         <Topbar />
 
-        <main className="flex-1 p-6 overflow-auto">{children}</main>
+        <main className="flex-1 p-6 overflow-auto">
+          {children}
+          <Toaster />
+        </main>
       </div>
     </div>
   );
