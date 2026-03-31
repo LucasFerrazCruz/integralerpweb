7dec8095c5a203a65698235670c389dbac119dd9
Integral Service <Contato@integralservice.net.br>
2026-03-31 06:10:03 -0300

    feat: implement layout

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,53 +1,146 @@
 "use client";
 
+import { useRole } from "@/hooks/useRole";
+import { cn } from "@/lib/utils";
+import {
+  ArrowLeftRight,
+  LayoutDashboard,
+  Package,
+  Settings,
+  ShoppingCart,
+  Tags,
+  Users,
+  Warehouse,
+} from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Item = {
+  label: string;
+  href: string;
+  icon: any;
+};
+
+function MenuItem({ item }: { item: Item }) {
+  const pathname = usePathname();
+  const isActive = pathname.startsWith(item.href);
+  const { isAdmin, isDistribuidor } = useRole();
+
+  return (
+    <Link
+      href={item.href}
+      className={cn(
+        "flex items-center gap-3 px-3 py-2 rounded-lg text-sm transition-all",
+        "hover:bg-gray-800 hover:text-white",
+        isActive ? "bg-gray-800 text-white font-medium" : "text-gray-400",
+      )}
+    >
+      <item.icon className="w-4 h-4" />
+      {item.label}
+    </Link>
+  );
+}
+
+function Section({ title, items }: { title: string; items: Item[] }) {
+  return (
+    <div className="space-y-2">
+      <p className="text-xs uppercase text-gray-500 px-2">{title}</p>
+
+      <div className="space-y-1">
+        {items.map((item) => (
+          <MenuItem key={item.href} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default function Sidebar() {
   return (
-    <aside className="w-[220px] bg-black text-white p-5">
-      <h2 className="text-lg font-bold">Integral ERP</h2>
+    <aside className="w-[260px] bg-zinc-950 text-white h-screen p-4 border-zinc-800 flex flex-col">
+      {/* LOGO */}
+      <div className="mb-6 px-2">
+        <h2 className="text-lg font-semibold tracking-tight">Integral ERP</h2>
+      </div>
 
-      <nav className="mt-6 space-y-4">
-        <div>
-          <Link href="/dashboard">Dashboard</Link>
-        </div>
+      <div className="flex flex-col gap-6">
+        <Section
+          title="Geral"
+          items={[
+            {
+              label: "Dashboard",
+              href: "/dashboard",
+              icon: LayoutDashboard,
+            },
+          ]}
+        />
 
-        <div className="mt-6 font-semibold">Catálogo</div>
-
-        <div>
-          <Link href="/catalogo/produtos">Produtos</Link>
-        </div>
-
-        <div>
-          <Link href="/catalogo/categorias">Categorias</Link>
-        </div>
-
-        <div className="mt-6 font-semibold">Estoque</div>
-
-        <div>
-          <Link href="/estoque/produtos">Centros</Link>
-        </div>
-
-        <div>
-          <Link href="/movimentacoes">Movimentações</Link>
-        </div>
-
-        <div className="mt-6 font-semibold">Vendas</div>
-
-        <div>
-          <Link href="/pedidos">Pedidos</Link>
-        </div>
-
-        <div>
-          <Link href="/clientes">Clientes</Link>
-        </div>
-
-        <div className="mt-6 font-semibold">Administração</div>
-
-        <div>
-          <Link href="/usuarios">Usuários</Link>
-        </div>
-      </nav>
+        <Section
+          title="Catálogo"
+          items={[
+            {
+              label: "Produtos",
+              href: "/catalogo/produtos",
+              icon: Package,
+            },
+            {
+              label: "Categorias",
+              href: "/catalogo/categorias",
+              icon: Tags,
+            },
+          ]}
+        />
+        <Section
+          title="Estoque"
+          items={[
+            {
+              label: "Estoque",
+              href: "/estoque/produtos",
+              icon: Warehouse,
+            },
+            {
+              label: "Movimentações",
+              href: "/estoque/movimentacoes",
+              icon: ArrowLeftRight,
+            },
+            {
+              label: "Transferências",
+              href: "/estoque/transferencias",
+              icon: ArrowLeftRight,
+            },
+            {
+              label: "Recebimento",
+              href: "/estoque/recebimento",
+              icon: ArrowLeftRight,
+            },
+          ]}
+        />
+        <Section
+          title="Vendas"
+          items={[
+            {
+              label: "Pedidos",
+              href: "/pedidos",
+              icon: ShoppingCart,
+            },
+            {
+              label: "Clientes",
+              href: "/clientes",
+              icon: Users,
+            },
+          ]}
+        />
+        <Section
+          title="Administração"
+          items={[
+            {
+              label: "Usuários",
+              href: "/usuarios",
+              icon: Settings,
+            },
+          ]}
+        />
+      </div>
     </aside>
   );
 }
--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
+import { useCarrinho } from "@/context/CarrinhoContext";
+import { ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
 
 export default function Topbar() {
   const { usuario } = useAuth();
+  const { quantidade, animando } = useCarrinho();
+  const router = useRouter();
 
   function logout() {
     localStorage.removeItem("token");
@@ -11,20 +18,32 @@ export default function Topbar() {
   }
 
   return (
-    <div
-      style={{
-        height: 60,
-        background: "#f5f5f5",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end",
-        padding: "0 20px",
-        borderBottom: "1px solid #ddd",
-      }}
-    >
-      <span style={{ marginRight: 20 }}>{usuario?.nome}</span>
+    <div className="h-16 bg-white border-b flex items-center justify-end px-6 gap-6">
+      {/* CARRINHO */}
+      <div
+        className="relative cursor-pointer"
+        onClick={() => router.push("/catalogo/carrinho")}
+      >
+        <ShoppingCart
+          className={`w-6 h-6 transition-transform ${
+            animando ? "scale-125" : "sacle-100"
+          }`}
+        />
 
-      <button onClick={logout}>Sair</button>
+        {quantidade > 0 && (
+          <Badge className="absolute -top-2 -right-2 bg-black text-white text-xs px-2 rounded-full">
+            {quantidade}
+          </Badge>
+        )}
+      </div>
+
+      {/* USUARIO */}
+      <span className="text-sm text-gray-700">{usuario?.nome}</span>
+
+      {/* LOGOUT */}
+      <Button variant="outline" onClick={logout}>
+        Sair
+      </Button>
     </div>
   );
 }
