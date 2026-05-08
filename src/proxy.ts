8cc3465c4f84b8ad8ec18fe89f43ce0ec58de7db
Integral Service <Contato@integralservice.net.br>
2026-05-08 07:37:37 -0300

    fix: test drop proxy.js

--- src/proxy.ts
+++
@@ -1,7 +0,0 @@
-// export { default } from "next-auth/middleware";
-
-// export const config = {
-//   matcher: ["/checkout/:path*", "/perfil/:path*"],
-// };
-
-export default function proxy() {} // Deixe uma função vazia só para não dar erro de import
