ff07703822677b1accce02a07734583e1054782a
LucasFerrazCruz <lucas.ferraz1906@gmail.com>
2026-05-19 17:20:11 -0300

    fix: responsive screen

--- src/app/(erp)/layout.tsx
+++ src/app/(erp)/layout.tsx
@@ -6,13 +6,17 @@ import { Header } from "@/components/layout/Header";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-gray-50 flex flex-col w-full overflow-x-hidden">
       {/* O Header agora contém toda a navegação */}
       <Header />
 
-      <main className="flex-1 w-full">
-        <div className="p-2 md:p-8 max-w-[1400px] mx-auto w-full">
-          <Suspense fallback={<div>Carregando...</div>}>{children}</Suspense>
+      <main className="flex-1 w-full flex flex-col">
+        <div className="p-4 md:p-8 max-w-[1440px] mx-auto w-full flex-1">
+          <Suspense
+            fallback={<div className="text-center py-10">Carregando...</div>}
+          >
+            {children}
+          </Suspense>
         </div>
       </main>
 
